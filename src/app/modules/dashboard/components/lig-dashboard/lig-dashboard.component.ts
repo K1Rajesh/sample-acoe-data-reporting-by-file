23af1a7ef3694e4d79c295c80b9f12a4e0aa86ee
Rahul Eppa <rahul.rowthi@gmail.com>
2023-09-27 12:03:45 +0530

    move filter  to lig dashboard filter

--- src/app/modules/dashboard/components/lig-dashboard/lig-dashboard.component.ts
+++ src/app/modules/dashboard/components/lig-dashboard/lig-dashboard.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnDestroy, OnInit  } from '@angular/core';
 import  { LigDashboardDataModel } from '../../models/lig-dashboard-data.model'
 import { LigDashboardModel } from '../../domain/models/lig-dashboard.model'
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-lig-dashboard',
@@ -12,9 +9,7 @@ import {map, startWith} from 'rxjs/operators';
   providers: [LigDashboardModel]
 })
 export class LigDashboardComponent implements OnInit , OnDestroy {
-  myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]> = new Observable<string[]>();
+
   constructor(private ligDashboardModel : LigDashboardModel) {
   }
   get headerColumns(): Array<string> {
@@ -41,15 +36,6 @@ export class LigDashboardComponent implements OnInit , OnDestroy {
   }
   ngOnInit(): void {
     this.ligDashboardModel.init();
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-  }
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
   public getSourceDataclickHandler(): void {
     this.ligDashboardModel.getSourceDataclickHandler();
@@ -62,7 +48,6 @@ export class LigDashboardComponent implements OnInit , OnDestroy {
     this.ligDashboardModel.dataManipulationSort(field, sortOrder)
   }
  
-
   public ngOnDestroy(): void {
     this.ligDashboardModel.destroy();
   }
