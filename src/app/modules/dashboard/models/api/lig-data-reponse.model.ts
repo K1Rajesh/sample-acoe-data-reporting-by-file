abbe42c2fe0712a7378288e4bff4e4b1f3c14809
Rahul Eppa <rahul.rowthi@gmail.com>
2023-09-27 18:07:42 +0530

    update endpoint, add api model

---
+++ src/app/modules/dashboard/models/api/lig-data-reponse.model.ts
@@ -0,0 +1,9 @@
+
+
+export type  FilterIModel =  {
+    LIGUserPersona :Array<string>,
+    LIGTaluka :Array<string>,
+    LIGState :Array<string>,
+    ChannelPartner : Array<string>,
+    BITerritory :Array<string>,
+}
--- src/app/modules/dashboard/services/lig-data.service.ts
+++ src/app/modules/dashboard/services/lig-data.service.ts
@@ -21,8 +21,14 @@ export class LigDataService{
 
     public getLigData():Observable<any>{
         //http://10.29.3.4:3003/saml-sp
-        return this.httpClient.post<any>("https://reportsdev.bpcl.co.in/pukipy/lig_data1",
-        {"email": "z_act_dev3@corp.bharatpetroleum.com", "month": "2023-08"})
+        // return this.httpClient.post<any>("https://reportsdev.bpcl.co.in/pukipy/lig_data1",
+        // {"email": "z_act_dev3@corp.bharatpetroleum.com", "month": "2023-08"})
+
+        return this.httpClient.post<any>("https://reportsdev.bpcl.co.in/pukipy/lig_data2",
+        {"email": "z_act_dev3@corp.bharatpetroleum.com",
+         "month": "2023-08",
+        "user_persona":"lubes_personalvechileowner",
+        "Bi_Sales_Area":"Rourkela Lubes"})
     }
 
 
