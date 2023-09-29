7e9e412ac210674f78562b02271d7d32dd7beacd
Rahul Eppa <rahul.rowthi@gmail.com>
2023-09-29 17:58:59 +0530

    mock data in seperate file creation

---
+++ src/app/modules/dashboard/models/api/sample-lig-data-request.model.ts
@@ -0,0 +1,14 @@
+import { LigDataRequestIModel } from './lig-data-request.model';
+
+export const MOCK_LIG_DATA_REQUEST: LigDataRequestIModel = {
+    "user": {
+      "email": "abhisekdatta@corp.bharatpetroleum.com"
+    },
+    "filters": {
+      "month": "2023-08",
+      "user_persona": "lubes_personalvehicleowner",
+      "taluka": "Kesinga",
+      "sap_cc_number": "117606.0",
+      "SALES_GROUP_NAME": "Rourkela Lubes"
+    }
+}
