803a3d0a7c7f5af728207de6edb738c7d4b8a1f0
Rahul Eppa <rahul.rowthi@gmail.com>
2023-10-02 15:35:02 +0530

    add filter model

---
+++ src/app/modules/dashboard/models/lig-dashboard-filter.model.ts
@@ -0,0 +1,9 @@
+import {FormControl} from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export interface FiterControlIModel {
+    filterControl : FormControl;
+    filterOptionsAll: Array<string>;
+    filterOptionsCurrent$: Observable<string[]> 
+
+}
